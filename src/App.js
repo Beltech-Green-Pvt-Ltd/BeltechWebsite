14e1df4a07d487c9cfb7ccf5cd5787ea0ab9c6fe
Ravi Maurya <ravi@beltech.ai>
2022-12-06 16:47:32 +0530

    Route is added

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ContactUs from "./container/ContactUs";
+import ScrollToTop from "./components/ScrollToTop";
+import SpinnerDiv from "./components/LoadingContainer";
 
 //Importing Component
 const Home = lazy(() => import("./container/HomePage"));
 const Carrer = lazy(() => import("./container/CarrerPage"));
 const Team = lazy(() => import("./container/TeamPage/"));
 const Product = lazy(() => import("./container/ProductPage"));
+const Apply  = lazy(() => import("./container/ApplyPage"));
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div><SpinnerDiv /></div>}>
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/career" element={<Carrer />} />
+          <Route path="/job/:jobId" element={<h1>jobPage</h1>} />
           <Route path="/team" element={<Team />}/>
           <Route path="/contactus" element={<ContactUs />}/>
           <Route path="/product" element={<Product />}/>
+          <Route path="/apply" element={<Apply />}/>
       </Routes>
       </Suspense>
       <Footer />
