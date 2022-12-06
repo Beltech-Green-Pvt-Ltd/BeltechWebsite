7cc9719eeb4f5857b0d96d2c194b90e4c97fa224
Ravi Maurya <ravi@beltech.ai>
2022-12-07 00:32:07 +0530

    Shadow in Header is added

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -9,6 +9,7 @@ export const MainContainer = styled.div`
     top:0;
     z-index: 100;
     background-color: white;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
 `;
 
 export const Container = styled.div`
