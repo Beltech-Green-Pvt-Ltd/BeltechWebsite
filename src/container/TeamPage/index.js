a0906240392a91329226e8b02eacc33fc133fcb3
Ravi Maurya <ravi@beltech.ai>
2022-12-12 12:16:42 +0530

    Team Page Mobile View

--- src/container/TeamPage/index.js
+++ src/container/TeamPage/index.js
@@ -22,14 +22,16 @@ import {
   AdName,
   AdDesg,
   AdExp,
+  CoreTeamContainer2,
+  TeamProfileContainer2,
 } from "./style";
 import AgastyaBellad from "../../assets/Agastya.png";
 import pinakee from "../../assets/Pinakee.jpeg";
 import sathisha from "../../assets/sathisha.jpeg";
-import rajesh from '../../assets/Rajesh.jpg';
-import ravi from '../../assets/Ravi.jpg';
-import ashish from '../../assets/Ashish.jpg';
-import robendas from '../../assets/Robendas.jpeg';
+import rajesh from "../../assets/Rajesh.jpg";
+import ravi from "../../assets/Ravi.jpg";
+import ashish from "../../assets/Ashish.jpg";
+import robendas from "../../assets/Robendas.jpeg";
 
 const TeamPage = () => {
   return (
@@ -63,6 +65,30 @@ const TeamPage = () => {
             </TeamProfileContainer>
           </TeamProfiles>
         </CoreTeamContainer>
+        <CoreTeamContainer2>
+          <TeamProfileContainer2>
+            <Image src={AgastyaBellad} />
+            <Name>Agastya Bellad</Name>
+            <Designation>Founder & CEO</Designation>
+            <CollegeText>Boston University</CollegeText>
+          </TeamProfileContainer2>
+        </CoreTeamContainer2>
+        <CoreTeamContainer2>
+          <TeamProfileContainer2>
+            <Image src={pinakee} />
+            <Name>Pinakee Biswas</Name>
+            <Designation>Co-Founder & CTO</Designation>
+            <CollegeText>IIT, Delhi</CollegeText>
+          </TeamProfileContainer2>
+        </CoreTeamContainer2>
+        <CoreTeamContainer2>
+          <TeamProfileContainer2>
+            <Image src={sathisha} />
+            <Name>Dr. Sathisha Basavaraju</Name>
+            <Designation>Artificial Intelligence Scientist</Designation>
+            <CollegeText>IIT, Guwahati</CollegeText>
+          </TeamProfileContainer2>
+        </CoreTeamContainer2>
         <AdvisoryBoardContainer>
           <HeadingContainer>
             <HeadingSubContainer>
@@ -72,38 +98,60 @@ const TeamPage = () => {
           </HeadingContainer>
           <AdvisoryBoardTeamContainer>
             <AdvisoryTeamCard>
-                <AdvisoryTeamImage src={ashish}/>
-                <AdvisoryTeamInfo>
-                    <AdName>Ashish Kashyap</AdName>
-                    <AdDesg>Founder and CEO of INDmoney.com Founder & Director of Finzoom.</AdDesg>
-                    <AdExp>Founder & X-CEO of ibibo (Goibibo & redBus) India's leading online travel group.</AdExp>
-                </AdvisoryTeamInfo>
+              <AdvisoryTeamImage src={ashish} />
+              <AdvisoryTeamInfo>
+                <AdName>Ashish Kashyap</AdName>
+                <AdDesg>
+                  Founder and CEO of INDmoney.com Founder & Director of Finzoom.
+                </AdDesg>
+                <AdExp>
+                  Founder & X-CEO of ibibo (Goibibo & redBus) India's leading
+                  online travel group.
+                </AdExp>
+              </AdvisoryTeamInfo>
             </AdvisoryTeamCard>
             <AdvisoryTeamCard>
-                <AdvisoryTeamImage src={rajesh}/>
-                <AdvisoryTeamInfo>
-                    <AdName>Rajesh Dembla</AdName>
-                    <AdDesg>Founder of Bidzapp Founder of Zoozle and Founder of Pacific Gaming Pvt. Ltd.</AdDesg>
-                    <AdExp>Rajesh Dembla holds more than 22 years of telecommunication industry experience.</AdExp>
-                </AdvisoryTeamInfo>
+              <AdvisoryTeamImage src={rajesh} />
+              <AdvisoryTeamInfo>
+                <AdName>Rajesh Dembla</AdName>
+                <AdDesg>
+                  Founder of Bidzapp Founder of Zoozle and Founder of Pacific
+                  Gaming Pvt. Ltd.
+                </AdDesg>
+                <AdExp>
+                  Rajesh Dembla holds more than 22 years of telecommunication
+                  industry experience.
+                </AdExp>
+              </AdvisoryTeamInfo>
             </AdvisoryTeamCard>
           </AdvisoryBoardTeamContainer>
           <AdvisoryBoardTeamContainer>
             <AdvisoryTeamCard>
-                <AdvisoryTeamImage src={ravi}/>
-                <AdvisoryTeamInfo>
-                    <AdName>Ravi Koulagi</AdName>
-                    <AdDesg>Global Director and CTO Global Public Sector Segment at Cisco.</AdDesg>
-                    <AdExp>Ravi Koulagi holds more than 25 years of software and telecommunication industry experience.</AdExp>
-                </AdvisoryTeamInfo>
+              <AdvisoryTeamImage src={ravi} />
+              <AdvisoryTeamInfo>
+                <AdName>Ravi Koulagi</AdName>
+                <AdDesg>
+                  Global Director and CTO Global Public Sector Segment at Cisco.
+                </AdDesg>
+                <AdExp>
+                  Ravi Koulagi holds more than 25 years of software and
+                  telecommunication industry experience.
+                </AdExp>
+              </AdvisoryTeamInfo>
             </AdvisoryTeamCard>
             <AdvisoryTeamCard>
-                <AdvisoryTeamImage src={robendas}/>
-                <AdvisoryTeamInfo>
-                    <AdName>Roben Dass</AdName>
-                    <AdDesg>Founder Chairman & Managing Director - KOOCHIE GLOBAL</AdDesg>
-                    <AdExp>Founder Chairman & Managing Director at KoochieGlobal a multinational conglomerate that has 24 offices & 9 state of the art OEM production units across the world.</AdExp>
-                </AdvisoryTeamInfo>
+              <AdvisoryTeamImage src={robendas} />
+              <AdvisoryTeamInfo>
+                <AdName>Roben Dass</AdName>
+                <AdDesg>
+                  Founder Chairman & Managing Director - KOOCHIE GLOBAL
+                </AdDesg>
+                <AdExp>
+                  Founder Chairman & Managing Director at KoochieGlobal a
+                  multinational conglomerate that has 24 offices & 9 state of
+                  the art OEM production units across the world.
+                </AdExp>
+              </AdvisoryTeamInfo>
             </AdvisoryTeamCard>
           </AdvisoryBoardTeamContainer>
         </AdvisoryBoardContainer>
--- src/container/TeamPage/style.js
+++ src/container/TeamPage/style.js
@@ -9,6 +9,9 @@ export const MainContainer = styled.div`
 export const Container = styled.div`
   max-width: 1580px;
   width: 100%;
+  @media (max-width: 1200px) {
+    padding-bottom: 50px;
+  }
 `;
 
 export const HeadingContainer = styled.div`
@@ -32,10 +35,20 @@ export const Circle = styled.div`
   top: 19%;
   left: -10%;
   z-index: -1;
+  @media (max-width: 760px) {
+    width: 60px;
+    height: 60px;
+  }
 `;
 
 export const HeadingText = styled.h1`
   font-size: 47px;
+  @media (max-width: 1200px) {
+    font-size: 40px;
+  }
+  @media (max-width: 760px) {
+    font-size: 35px;
+  }
 `;
 
 export const CoreTeamContainer = styled.div`
@@ -44,6 +57,26 @@ export const CoreTeamContainer = styled.div`
   min-height: 610px;
   margin-top: 40px;
   background: linear-gradient(180deg, #ffffff 50%, #006ae9 50%);
+  @media (max-width: 1200px) {
+    min-height: 480px;
+    margin-top: 25px;
+  }
+  @media (max-width: 790px) {
+    display: none;
+  }
+`;
+
+export const CoreTeamContainer2 = styled.div`
+  position: relative;
+  background-color: ${Colors.primaryColor};
+  min-height: 400px;
+  margin-top: 30px;
+  background: linear-gradient(180deg, #ffffff 50%, #006ae9 50%);
+  display: flex;
+  justify-content: center;
+  @media (min-width: 791px) {
+    display: none;
+  }
 `;
 
 export const TeamProfileContainer = styled.div`
@@ -60,6 +93,35 @@ export const TeamProfileContainer = styled.div`
   align-items: center;
   margin-right: 30px;
   margin-left: 30px;
+  @media (max-width: 1200px) {
+    width: 250px;
+    height: 300px;
+  }
+`;
+
+export const TeamProfileContainer2 = styled.div`
+  width: 340px;
+  height: 400px;
+  left: 33%;
+  top: 40px;
+  background: #ffffff;
+  border: 1px solid #f0f0f0;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  @media (max-width: 1200px) {
+    width: 250px;
+    height: 300px;
+  }
+  @media (max-width: 538px) {
+    left: 25%;
+  }
+  @media (max-width: 438px) {
+    left: 20%;
+  }
 `;
 
 export const TeamProfiles = styled.div`
@@ -71,6 +133,10 @@ export const Image = styled.img`
   width: 160px;
   height: 160px;
   border-radius: 90px;
+  @media (max-width: 1200px) {
+    width: 130px;
+    height: 130px;
+  }
 `;
 
 export const Name = styled.div`
@@ -105,6 +171,13 @@ export const AdvisoryBoardTeamContainer = styled.div`
     justify-content: space-around;
     margin-top: 167px;
     margin-bottom: 117px;
+    @media (max-width: 1100px) {
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-top: 0px;
+      margin-bottom: 0px;
+    }
 `;
 
 export const AdvisoryTeamCard = styled.div`
@@ -112,6 +185,16 @@ export const AdvisoryTeamCard = styled.div`
   width: 580px;
   background-color: #f4f4f4;
   display: flex;
+  @media (max-width: 1200px) {
+    max-height: 300px;
+    width: 520px;
+  }
+  @media (max-width: 1100px) {
+    margin-top: 140px;
+  }
+  @media (max-width: 580px) {
+    max-width: 400px;
+  }
 `;
 
 export const AdvisoryTeamImage = styled.img`
@@ -120,25 +203,38 @@ export const AdvisoryTeamImage = styled.img`
   position: relative;
   left: 20px;
   bottom: 70px;
+  @media (max-width: 1200px) {
+    width: 100x;
+    height: 240px;
+  }
+  @media (max-width: 580px) {
+    width: 132px;
+    height: 193px;
+    bottom: 50px;
+  }
 `;
 
 export const AdvisoryTeamInfo = styled.div`
   margin-left: 50px;
+  @media (max-width: 580px) {
+    margin-left: 35px;
+  }
 `;
 
 export const AdName = styled.div`
   font-weight: 700;
   font-size: 24px;
   line-height: 29px;
-  letter-spacing: -0.3px;
   margin-top: 21px;
+  @media (max-width: 580px) {
+    font-size: 20px;
+  }
 `;
 
 export const AdDesg = styled.div`
   font-weight: 600;
   font-size: 16px;
   line-height: 19px;
-  letter-spacing: -0.3px;
   color: #006ae9;
   margin-top: 15px;
   width: 232px;
@@ -148,8 +244,11 @@ export const AdExp = styled.div`
   font-weight: 500;
   font-size: 16px;
   line-height: 19px;
-  letter-spacing: -0.3px;
   color: #000000;
   margin-top: 70px;
   width: 231px;
+  @media (max-width: 1100px) {
+    margin-top: 50px;
+    padding-bottom: 10px;
+  }
 `;
