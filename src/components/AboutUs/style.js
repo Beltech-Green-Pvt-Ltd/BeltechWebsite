054fefd19618c41f31b20bd9ebe56976ec94a491
Ravi Maurya <ravi@beltech.ai>
2022-12-13 23:20:08 +0530

    About Us mobile View

--- src/components/AboutUs/style.js
+++ src/components/AboutUs/style.js
@@ -18,37 +18,57 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0px 97px 0px 97px;
-
 `;
 
 export const HeadingText = styled.div`
-    font-size: 50px;
-    color: #000A55;
-    margin-bottom: 105px;
-    font-weight: 700;
-    @media (max-width: 1330px) {
-        font-size: 45px;
-    }
+  font-size: 50px;
+  color: #000a55;
+  margin-bottom: 105px;
+  font-weight: 700;
+  @media (max-width: 1330px) {
+    font-size: 45px;
+  }
+  @media (max-width: 1007px) {
+    margin-bottom: 60px;
+    margin-top: 60px;
+  }
 `;
 
 export const ContentContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  @media (max-width: 1007px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const AboutUsContent = styled.div`
-    display: flex;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 440px;
+  @media (max-width: 1007px) {
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    max-width: 440px;
+    margin-bottom: 35px;
+  }
 `;
 
 export const Image = styled.img`
-    width: 104px;
+  width: 104px;
+  @media (max-width: 1007px) {
+    width: 80px;
+  }
 `;
 
 export const Text = styled.div`
-    font-size: 22px;
-    margin-left: 25px;
+  font-size: 22px;
+  margin-left: 25px;
+  @media (max-width: 1007px) {
+    text-align: center;
+  }
 `;
